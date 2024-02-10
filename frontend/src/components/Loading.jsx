98f3feb1a02054213f97284d99508dff1e348b5b
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-02-10 02:39:42 -0800

    cool loading screen

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import earth from "../assets/earth.gif";
 
 const Loading = () => {
-	return (
-		<div
-			style={{
-				width: "100vw",
-				height: "100vh",
-				backgroundColor: "black",
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				color: "white",
-				fontSize: "24px",
-			}}
-		>
-			Loading...
-		</div>
-	);
+    return (
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "black",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                fontSize: "24px",
+            }}
+        >
+            <img
+                src={earth}
+                style={{
+                    height: "180px",
+                    width: "180px",
+                    marginBottom: "8px",
+                }}
+            />
+            Loading...
+        </div>
+    );
 };
 
 export default Loading;
